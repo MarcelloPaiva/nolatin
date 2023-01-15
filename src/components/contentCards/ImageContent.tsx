24341c11ab265583022560e05a7d1c0559746ba3
MarcelloPaiva <marcello.paiva@gmail.com>
2023-01-15 10:14:22 -0500

    set up image preview

--- src/components/contentCards/ImageContent.tsx
+++ src/components/contentCards/ImageContent.tsx
@@ -1,6 +1,12 @@
 import Input from "../Input"
 import { Label, Text } from ".."
 import { ContentProps } from "./ContentProps"
+import styled from "styled-components"
+
+const Image = styled.img`
+  border: 1px solid #a8a8a8;
+  width: 100%;
+`
 
 export default function ImageContent({
   state: { edit, title, url, description, id },
@@ -23,6 +29,7 @@ export default function ImageContent({
           <Text>{title}</Text>
           <Label>Source URL</Label>
           <Text>{url}</Text>
+          <Image src={url} alt={title} />
           <Label>Caption</Label>
           <Text>{description}</Text>
         </>

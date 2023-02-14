6a626804f34fdcf4d944bdda754e4026f2499b5d
MarcelloPaiva <marcello.paiva@gmail.com>
2023-02-14 10:06:01 -0500

    change logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
-import { Menu, Slash } from "react-feather"
+import { Menu } from "react-feather"
 import IconButton from "./IconButton"
 import styled from "styled-components"
+import NoLatin from "../assets/svgs/nolatinLogo.svg"
 
 const Header = styled.header`
   width: auto;
@@ -8,26 +9,27 @@ const Header = styled.header`
   justify-content: space-between;
   padding: 1rem 1rem 0;
   background: var(--secondary-20);
+  align-items: center;
 `
-
 const Row = styled.div`
   display: flex;
   justify-content: space-between;
+`
+const Logo = styled.img`
+  width: 200px;
+  border: none;
+  margin: 0;
 `
 
 export default function NavBar() {
   return (
     <Header>
-      <IconButton
-        size={60}
-        icon={Slash}
-        onClick={() => {}}
-        aria="No Latin - A shift-left tool for product designers"
-        label="No Latin"
-        color="var( --secondary-80)"
-        styles="color: var( --secondary-80);"
-        link="/"
-      />
+      <a href="/">
+        <Logo
+          src={NoLatin}
+          alt="No Latin - A shift-left tool for product designers"
+        />
+      </a>
       <Row>
         <IconButton
           size={24}

e0ccf06790c39c8142fc48631ae5b3f05f96b3e7
Marcello Paiva <marcello.paiva@gmail.com>
2024-09-03 18:04:03 -0400

    added success and error hanfling

--- src/components/generated/Export.tsx
+++ src/components/generated/Export.tsx
@@ -28,16 +28,20 @@ const ExportForm = ({ onClose }: ExportFormProps) => {
     json_content: "",
     emailaddress: "",
   })
-  const [success, setSuccess] = useState(false)
+  const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [postSuccess, setPostSuccess] = useState(false)
   const [error, setError] = useState<string>("")
 
   const handleUpdate = async () => {
     try {
       await apiUpdate(updateData)
+      setUpdateSuccess(true)
     } catch (error) {
       let message
       if (error instanceof Error) message = error.message
+      else message = String(error)
       console.warn("ERROR", message)
+      setError(message)
     }
   }
 
@@ -67,14 +71,14 @@ const ExportForm = ({ onClose }: ExportFormProps) => {
       ) {
         setUpdate(result)
       } else {
-        setSuccess(true)
+        setPostSuccess(true)
       }
     } catch (error) {
       let message
       if (error instanceof Error) message = error.message
       else message = String(error)
       if (message.includes("Your link was created successfully")) {
-        setSuccess(true)
+        setPostSuccess(true)
       } else if (
         message.includes(
           "Friendly name already exists. Would you like to update"
@@ -96,79 +100,97 @@ const ExportForm = ({ onClose }: ExportFormProps) => {
 
   return (
     <div>
-      {success ? (
+      {updateSuccess ? (
         <>
           <EndRow>
             <IconButton
               icon={X}
-              aria="Cancel Edit"
-              label="Cancel"
+              aria="Close Modal"
+              label="Close"
               onClick={onClose}
             />
           </EndRow>
-          <p>
-            Link successfully created, click the link to add it to your
-            clipboard
-          </p>
-          <CopyButton onClick={() => navigator.clipboard.writeText(linkText)}>
-            <p>{linkText}</p>
-          </CopyButton>
+          <p>Friendly name successfully updated.</p>
         </>
       ) : (
         <>
-          <EndRow>
-            <IconButton
-              icon={X}
-              aria="Cancel Edit"
-              label="Cancel"
-              styles="margin-right: 24px"
-              onClick={onClose}
-            />
-            <IconButton
-              icon={Send}
-              color="var(--button-save-label)"
-              aria="Send"
-              label="Send"
-              styles="background:var(--share-bg);border-radius:4px;width:60px;"
-              onClick={handleSubmit}
-            />
-          </EndRow>
+          {postSuccess ? (
+            <>
+              <EndRow>
+                <IconButton
+                  icon={X}
+                  aria="Close Modal"
+                  label="Close"
+                  onClick={onClose}
+                />
+              </EndRow>
+              <p>
+                Link successfully created, click the link to add it to your
+                clipboard
+              </p>
+              <CopyButton
+                onClick={() => navigator.clipboard.writeText(linkText)}
+              >
+                <p>{linkText}</p>
+              </CopyButton>
+            </>
+          ) : (
+            <>
+              <EndRow>
+                <IconButton
+                  icon={X}
+                  aria="Cancel Edit"
+                  label="Cancel"
+                  styles="margin-right: 24px"
+                  onClick={onClose}
+                />
+                <IconButton
+                  icon={Send}
+                  color="var(--button-save-label)"
+                  aria="Send"
+                  label="Send"
+                  styles="background:var(--share-bg);border-radius:4px;width:60px;"
+                  onClick={handleSubmit}
+                />
+              </EndRow>
 
-          <p>
-            Add a <span className="friendlyName">friendly name</span> for your
-            URL.
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="friendlyName">Friendly Name:</label>
-              <input
-                type="text"
-                id="friendlyName"
-                value={friendlyName}
-                onChange={(event) => setFriendlyName(event.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="emailAddress">Email Address:</label>
-              <input
-                type="email"
-                id="emailAddress"
-                value={emailAddress}
-                onChange={(event) => setEmailAddress(event.target.value)}
-                required
-              />
-            </div>
-            {update && (
-              <div>
-                <p>{update}</p>
-                <Button label="Update" onClick={handleUpdate}>
-                  Update
-                </Button>
-              </div>
-            )}
-            {error && <p aria-live="polite">Error: {error}</p>}
-          </form>
+              <p>
+                Add a <span className="friendlyName">friendly name</span> for
+                your URL.
+              </p>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="friendlyName">Friendly Name:</label>
+                  <input
+                    type="text"
+                    id="friendlyName"
+                    value={friendlyName}
+                    onChange={(event) => setFriendlyName(event.target.value)}
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="emailAddress">Email Address:</label>
+                  <input
+                    type="email"
+                    id="emailAddress"
+                    value={emailAddress}
+                    onChange={(event) => setEmailAddress(event.target.value)}
+                    required
+                  />
+                </div>
+                {update && (
+                  <div>
+                    <p>{update}</p>
+                    <Button label="Update" onClick={handleUpdate}>
+                      Update
+                    </Button>
+                  </div>
+                )}
+                {error && <p aria-live="polite">Error: {error}</p>}
+              </form>
+            </>
+          )}
         </>
       )}
     </div>
